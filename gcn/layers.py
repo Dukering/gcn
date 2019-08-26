9bc6e21c99c702a38166e672c1159f3d91b70de7
Dukering <40348988+Dukering@users.noreply.github.com>
2019-08-26 14:49:56 +0800

    Update layers.py

--- gcn/layers.py
+++ gcn/layers.py
@@ -1,6 +1,6 @@
 from gcn.inits import *
 import tensorflow as tf
-
+#tf定义了tf.app.flags，用于支持接受命令行传递参数，相当于接受argv
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
@@ -22,8 +22,8 @@ def sparse_dropout(x, keep_prob, noise_shape):
     """Dropout for sparse tensors."""
     random_tensor = keep_prob
     random_tensor += tf.random_uniform(noise_shape)
-    dropout_mask = tf.cast(tf.floor(random_tensor), dtype=tf.bool)
-    pre_out = tf.sparse_retain(x, dropout_mask)
+    dropout_mask = tf.cast(tf.floor(random_tensor), dtype=tf.bool)   #数据类型转换
+    pre_out = tf.sparse_retain(x, dropout_mask)       #在x中，对应于dropout——mask位置上为True的留下
     return pre_out * (1./keep_prob)
 
 
@@ -51,16 +51,16 @@ class Layer(object):
         _log_vars(): Log all variables
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs):     #这两个是python中的可变参数。*args表示任何多个无名参数，它是一个tuple；**kwargs表示关键字参数，它是一个 dict。
         allowed_kwargs = {'name', 'logging'}
         for kwarg in kwargs.keys():
             assert kwarg in allowed_kwargs, 'Invalid keyword argument: ' + kwarg
-        name = kwargs.get('name')
+        name = kwargs.get('name')                     #返回name键的值
         if not name:
-            layer = self.__class__.__name__.lower()
-            name = layer + '_' + str(get_layer_uid(layer))
+            layer = self.__class__.__name__.lower()                #获取类名，并转为小写字母
+            name = layer + '_' + str(get_layer_uid(layer))               #获取完整类名，包含编号
         self.name = name
-        self.vars = {}
+        self.vars = {}                                        #存储参数
         logging = kwargs.get('logging', False)
         self.logging = logging
         self.sparse_inputs = False
@@ -71,7 +71,7 @@ class Layer(object):
     def __call__(self, inputs):
         with tf.name_scope(self.name):
             if self.logging and not self.sparse_inputs:
-                tf.summary.histogram(self.name + '/inputs', inputs)
+                tf.summary.histogram(self.name + '/inputs', inputs)                  #显示直方图信息
             outputs = self._call(inputs)
             if self.logging:
                 tf.summary.histogram(self.name + '/outputs', outputs)
@@ -82,7 +82,7 @@ class Layer(object):
             tf.summary.histogram(self.name + '/vars/' + var, self.vars[var])
 
 
-class Dense(Layer):
+class Dense(Layer):                #全链接层继承Layer类
     """Dense layer."""
     def __init__(self, input_dim, output_dim, placeholders, dropout=0., sparse_inputs=False,
                  act=tf.nn.relu, bias=False, featureless=False, **kwargs):
@@ -102,17 +102,17 @@ class Dense(Layer):
         self.num_features_nonzero = placeholders['num_features_nonzero']
 
         with tf.variable_scope(self.name + '_vars'):
-            self.vars['weights'] = glorot([input_dim, output_dim],
+            self.vars['weights'] = glorot([input_dim, output_dim],                    #生成参数变量
                                           name='weights')
             if self.bias:
                 self.vars['bias'] = zeros([output_dim], name='bias')
 
         if self.logging:
-            self._log_vars()
+            self._log_vars()                        #生成参数的柱形图
 
-    def _call(self, inputs):
+    def _call(self, inputs):                          #进行运算
         x = inputs
-
+        
         # dropout
         if self.sparse_inputs:
             x = sparse_dropout(x, 1-self.dropout, self.num_features_nonzero)
@@ -129,7 +129,7 @@ class Dense(Layer):
         return self.act(output)
 
 
-class GraphConvolution(Layer):
+class GraphConvolution(Layer):                      
     """Graph convolution layer."""
     def __init__(self, input_dim, output_dim, placeholders, dropout=0.,
                  sparse_inputs=False, act=tf.nn.relu, bias=False,
@@ -176,7 +176,7 @@ class GraphConvolution(Layer):
                 pre_sup = dot(x, self.vars['weights_' + str(i)],
                               sparse=self.sparse_inputs)
             else:
-                pre_sup = self.vars['weights_' + str(i)]
+                pre_sup = self.vars['weights_' + str(i)]                            #D**-1*A*X
             support = dot(self.support[i], pre_sup, sparse=True)
             supports.append(support)
         output = tf.add_n(supports)
